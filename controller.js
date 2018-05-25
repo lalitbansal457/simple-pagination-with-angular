d47b579320a778ba7fe162108071bee0eab255a7
lalit bansal <lalit.bansal457@gmail.com>
2018-05-25 15:23:27 +0530

    Adding comments.

--- controller.js
+++ controller.js
@@ -3,7 +3,8 @@ var app = angular.module('myApp', []);
 app.controller('paginationCtrl', function($scope){
 		var max = 1000
 		$scope.arr = [];
-
+		
+		// create static array for pagination
 		for(i = 1; i<=max; i++) {
 			$scope.arr.push(i);
 		}
@@ -11,9 +12,11 @@ app.controller('paginationCtrl', function($scope){
 		$scope.currentPage = 0;
 		$scope.pageSize = 20;
 
+		// calculate total number of pages.
 		$scope.numberOfpages = function(){
 			return Math.ceil($scope.arr.length/ $scope.pageSize);
 		}
+		
 		$scope.range = function() {
 			$scope.ret = [];
 			for(i=0; i<$scope.numberOfpages(); i++) {
@@ -23,12 +26,13 @@ app.controller('paginationCtrl', function($scope){
 		$scope.range();
 		console.log($scope.ret)
 
+		// set cuurent page index
 		$scope.setPage = function(index) {
-			console.log(index)
 			$scope.currentPage = index;
 		}
 });
 
+// Filter for data
 app.filter('pagination', function(){
 	return function (input, start) {
 		start = +start;
